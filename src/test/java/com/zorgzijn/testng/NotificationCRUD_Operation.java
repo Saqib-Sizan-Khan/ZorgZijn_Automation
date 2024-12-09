74851e96485f81a3feefd88c48ea4a0985da690c
Saqib Sizan Khan <ssk123098@gmail.com>
2024-12-09 12:31:07 +0600

    Comment added

--- src/test/java/com/zorgzijn/testng/NotificationCRUD_Operation.java
+++ src/test/java/com/zorgzijn/testng/NotificationCRUD_Operation.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.openqa.selenium.JavascriptExecutor;
 
 public class NotificationCRUD_Operation {
     WebDriver driver;
@@ -23,7 +22,8 @@ public class NotificationCRUD_Operation {
 
     @Test
     public void notificationCRUD() throws Exception {
-        driver.get(baseUrl + "/settings/profile");
+        //login automation
+        driver.get(baseUrl + "/auth/login");
         Thread.sleep(2000);
 
         driver.findElement(By.id("E-mailadres")).sendKeys("ssk123098@gmail.com");
@@ -31,15 +31,18 @@ public class NotificationCRUD_Operation {
         driver.findElement(By.id("remember")).click();
         Thread.sleep(2000);
 
+        //Navigate to notification tab
         driver.findElement(By.xpath("//button")).click();
         Thread.sleep(3000);
 
         driver.findElement(By.xpath("//li[2]/a")).click();
         Thread.sleep(2000);
 
+        //open notification creation form
         driver.findElement(By.xpath("//button")).click();
         Thread.sleep(3000);
 
+        //Fill up 1st step
         driver.findElement(By.id("Titel")).sendKeys("Automation Notification");
         driver.findElement(By.xpath("//div[@class='ck ck-editor__main']/div")).sendKeys("This is a automation text");
 
@@ -51,14 +54,19 @@ public class NotificationCRUD_Operation {
         driver.findElement(By.xpath("//form/div/button[1]")).click();
         Thread.sleep(2000);
 
+        //Choose Employee
         driver.findElement(By.id("simple-search")).sendKeys("Sizan");
         Thread.sleep(2000);
 
         driver.findElement(By.xpath("//li[2]//input")).click();
         Thread.sleep(1000);
 
+        //Confirm notification
         driver.findElement(By.xpath("//submit-button/button")).click();
         Thread.sleep(2000);
+
+
+
     }
 
     @AfterTest
