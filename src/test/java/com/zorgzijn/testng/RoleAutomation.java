819a5132327a21fa3a7182645b0e356a9d53c513
Saqib Sizan Khan <ssk123098@gmail.com>
2024-12-11 14:32:40 +0600

    role automation code refactor

--- src/test/java/com/zorgzijn/testng/RoleAutomation.java
+++ src/test/java/com/zorgzijn/testng/RoleAutomation.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 public class RoleAutomation extends AutomationSetupClass {
 
     private void clickElement(By locator) throws InterruptedException {
